b7bbef1a77994e073869ac60b9ddddf6572eaee8
dkatonn <dimasberightback@gmail.com>
2025-01-04 00:50:52 +0700

    user login ayam bakar

--- header.php
+++ header.php
@@ -49,8 +49,8 @@
             </ul>
             <form action="#" class="font-size-14 font-rale">
                     <div class="container mb-2 no-decoration">
-                        <a href="#" class="px-3 border-right border-left text-white no-decoration">Login</a>
-                        <a href="#" class="px-3 border-right text-white">Daftar Sekarang</a>
+                        <a href="./login/login.php" class="px-3 border-right border-left text-white no-decoration">Login</a>
+                        <a href="./login/login.php" class="px-3 border-right text-white">Daftar Sekarang</a>
                         <a href="cart.php" class="py-2 rounded-pill color-primary-bg">
                             <span class="font-size-16 px-2 text-white"><i class="fas fa-shopping-cart"></i></span>
                             <span class="px-3 py-2 rounded-pill text-dark bg-light"><?php echo count($product->getData('cart')); ?></span>
